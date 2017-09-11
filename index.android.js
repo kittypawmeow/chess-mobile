9bb9b9da76fd674d76e8fe1023d3d4e47328f3e4
Chuck Valenza <fallencx@gmail.com>
2017-09-11 19:33:10 -0400

    Added aesthetic 1

--- index.android.js
+++ index.android.js
@@ -1,53 +1,161 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * 
  */
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
 
 export default class chessmobile extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Chessboard/>
+				<Pieces/>
+			</View>
+		);
+	}
 }
 
+var ChessRow1 = React.createClass({
+	render: function() {
+		return (
+			<View style={styles.row}>
+				<View style={styles.black_square}/>
+				<View style={styles.white_square}/>
+				<View style={styles.black_square}/>
+				<View style={styles.white_square}/>
+				<View style={styles.black_square}/>
+				<View style={styles.white_square}/>
+				<View style={styles.black_square}/>
+				<View style={styles.white_square}/>
+			</View>
+		)
+	}
+});
+
+var ChessRow2 = React.createClass({
+	render: function() {
+		return (
+			<View style={styles.row}>
+				<View style={styles.white_square}/>
+				<View style={styles.black_square}/>
+				<View style={styles.white_square}/>
+				<View style={styles.black_square}/>
+				<View style={styles.white_square}/>
+				<View style={styles.black_square}/>
+				<View style={styles.white_square}/>
+				<View style={styles.black_square}/>
+			</View>
+		)
+	}
+});
+
+var Chessboard = React.createClass({
+	render: function() {
+		return (
+			<View>
+					<ChessRow1/>
+					<ChessRow2/>
+					<ChessRow1/>
+					<ChessRow2/>
+					<ChessRow1/>
+					<ChessRow2/>
+					<ChessRow1/>
+					<ChessRow2/>
+			</View>
+		)
+	}
+});
+
+var Pieces = React.createClass({
+	render: function() {
+		return (
+			<View style={styles.row}>
+				<Pawn/>
+				<Pawn/>
+				<Pawn/>
+				<Pawn/>
+				<Pawn/>
+				<Pawn/>
+				<Pawn/>
+				<Pawn/>
+			</View>
+		);
+	}
+});
+
+var Pawn = React.createClass({
+	render: function() {
+		return (
+			<View>
+				<View style={components.pawn_top}>
+					<View style={pawn_head('black')}/>
+					<View style={pawn_body('black')}/>
+				</View>
+			</View>
+		)
+	}
+});
+
+var pawn_head = function(c) {
+	return {
+		top: 5,
+		width: 10,
+		height: 10,
+		borderRadius: 100 / 2,
+		backgroundColor: c,
+	}
+}
+
+var pawn_body = function(c) {
+	return {
+		width: 0,
+		height: 0,
+		backgroundColor: 'transparent',
+		borderStyle: 'solid',
+		borderLeftWidth: 10,
+		borderRightWidth: 10,
+		borderBottomWidth: 20,
+		borderLeftColor: 'transparent',
+		borderRightColor: 'transparent',
+		borderBottomColor: c
+	}
+}
+
+const components = StyleSheet.create({
+	pawn_top: {
+		width: 20,
+		height: 20,
+		alignItems: 'center',
+	},
+});
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#999999',
+	},
+	row: {
+		flexDirection: 'row',
+		height: 50,
+	},
+	black_square: {
+		width: 50,
+		height: 50,
+		backgroundColor: 'black'
+	},
+	white_square: {
+		width: 50,
+		height: 50,
+		backgroundColor: 'white'
+	},
 });
 
 AppRegistry.registerComponent('chessmobile', () => chessmobile);
